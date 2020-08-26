c12b6e31ba1720c44b5dbce98a717f4edb532f0f
Suri Feng <0924suri@gmail.com>
2020-08-26 20:00:25 +0800

    Finished graded part in ex1

--- machine-learning-ex/ex1/gradientDescent.m
+++ machine-learning-ex/ex1/gradientDescent.m
@@ -16,9 +16,10 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
+    temp1 = theta(1,1) - alpha/m*sum(X*theta - y);
+    temp2 = theta(2,1) - alpha/m*sum((X*theta - y).*X(:,2));
+    theta(1,1) = temp1;
+    theta(2,1) = temp2;
 
 
 
