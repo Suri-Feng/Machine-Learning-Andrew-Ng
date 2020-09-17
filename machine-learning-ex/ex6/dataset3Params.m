d04684179f50726ce59dde943def69aee2cc922e
Suri Feng <0924suri@gmail.com>
2020-09-17 08:35:42 +0800

    Finished ex6

--- machine-learning-ex/ex6/dataset3Params.m
+++ machine-learning-ex/ex6/dataset3Params.m
@@ -23,10 +23,22 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+C_vec =  [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
+sigma_vec = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
+lowest_error = 1;
 
-
-
-
+for i = 1:length(C_vec)
+    for j = 1:length(sigma_vec)
+        model= svmTrain(X, y, C_vec(i), @(x1, x2) gaussianKernel(x1, x2, sigma_vec(j))); 
+        predictions = svmPredict(model, Xval);
+        error = mean(double(predictions ~= yval));
+        if error < lowest_error
+            lowest_error = error;
+            C = C_vec(i);
+            sigma = sigma_vec(j);
+        end
+    end
+end
 
 
 % =========================================================================
